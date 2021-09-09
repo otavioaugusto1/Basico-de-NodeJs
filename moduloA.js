88b4219d9e6217f6e9eea40561488025c6cb31d1
Otávio Augusto Gonçalves Nogueira <otavio.a.nogueira@hotmail.com>
2021-09-09 09:57:03 -0300

    Iniciando

---
+++ moduloA.js
@@ -0,0 +1,4 @@
+// maneiras de exportar
+this.ola = "Fala, pessoal."
+exports.bemVindo = "Bem vindo ao node"
+module.exports.ateLogo = "Até o próximo exemplo"
---
+++ moduloB.js
@@ -0,0 +1,7 @@
+// outra maneira de exportar
+module.exports = {
+    bomDia: "Bom dia",
+    boaNoite(){
+        return "Boa noite"
+    }
+}
